d7c003af9313653203dd63d8245c8b247c1be5b8
Garrick Henne <garrickhenne@outlook.com>
2024-01-09 19:07:45 -0800

    Install react router dom, create route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './views/Home'
 
 function App() {
 
   return (
-    <>
-      <h1>Meditation Timer</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
