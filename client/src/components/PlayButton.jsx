f37552504f13de1b489abd3233d9696833549448
Garrick Henne <garrickhenne@outlook.com>
2024-01-10 20:20:26 -0800

    Add play and stop components

--- client/src/components/PlayButton.jsx
+++ client/src/components/PlayButton.jsx
@@ -1,8 +1,8 @@
+import playSVG from '../assets/play-fill.svg';
+
 const PlayButton = () => {
   return (
-    <div>
-      <button>Play</button>
-    </div>
+    <button className='flex items-center justify-center h-16 w-16 rounded-full bg-white'><img className='h-1/2 w-1/2' src={ playSVG } alt='play'/></button>
   );
 }
 
---
+++ client/src/components/StopButton.jsx
@@ -0,0 +1,11 @@
+const StopButton = () => {
+  return (
+    <button>
+      <div className='flex items-center justify-center h-16 w-16 rounded-full bg-white'>
+        <div className='h-1/3 w-1/3 bg-[#242424] rounded-sm'></div>
+      </div>
+    </button>
+  );
+}
+
+export default StopButton;
