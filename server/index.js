2bca3e881eb2a839ee0c658c087ac61e161d4ae3
Garrick Henne <garrickhenne@outlook.com>
2024-01-18 11:54:51 -0800

    Create boiler server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.get('/user', (req, res) => {
+  res.send('user');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
